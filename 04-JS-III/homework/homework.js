d3b7863f79c3ff660f46bed01b2741d682e7d67d
unknown <nahunico.r@gmail.com>
2022-02-23 02:00:41 -0300

    homework JSIII-finished

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -222,16 +222,22 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  var array = [];
+  var bank = [];
   var suma =numero;
-  for(var i=0; i<10; i++){
+  for(i=0; i < 10; i++){
     suma = suma + 2;
-    if(suma=== i) break;
-    return "Se interrumpió la ejecución";
+    if(suma === i){
+      break;
+    }else{
+      bank.push(suma);
+    }
+   }
+   if(i < 10){
+     return 'Se interrumpió la ejecución';
+   }
+   return bank;
   }
-  return array;  
-  }
-}
+
 
 
 function continueStatement(numero) {
@@ -241,7 +247,31 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for(let i = 0; i < 10; i++){
+    if( i === 5) continue;
+    else{
+      suma = suma + 2;
+      array.push(suma);
+    }
+  }
+  return array;
 }
+ /* var number = [];
+  var i=0;
+  var n = 6;
+  while(i < 10){
+    i= i +1;
+   number = numero + 2;
+    if(i === 5){
+   continue;
+  }
+
+    n += i;
+  }
+  return number;
+}*/
 
 
 // No modificar nada debajo de esta línea
